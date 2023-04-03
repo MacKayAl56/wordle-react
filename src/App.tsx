d9abd26c1a4448a52dcb01cadf6c02bd794aeda1
MacKayAl56 <mackayal@mail.gvsu.edu>
2023-04-03 12:33:04 -0400

    Made wordle grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
-// import './App.css'
+import './App.css'
 import Wordle from "./Wordle"
 function App() {
+  const index = [];
+  var i = 0;
+  for (i = 0; i < 30; i++){
+    index.push(i);
+  }
 
   return <>
-    <h1>My Wordle Clone</h1>
+    <h1 className="header">Wordle Clone</h1>
+    <h2 className="subheader">By: Kyle Smigelski and Alexandra MacKay</h2>
+    <div className="field">
+      <div className="grid">
+      {
+        index.map((pos:number) =>
+        <div className="box"></div>)
+      }
+
+      </div>
+    </div>
+
     <Wordle/>
     </>
 }
