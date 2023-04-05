db8792f93fef8eaa6ee7ebaaedf612d6ad099fbb
MacKayAl56 <mackayal@mail.gvsu.edu>
2023-04-05 12:26:12 -0400

    Added brief report

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,18 @@ function App() {
       </div>
     </div>
 
-    <Wordle/>
-    </>
+    <Wordle />
+    <div>
+      <h1 className="header">Report</h1>
+      <h2 className="subheader">In order to build the grid of letters, we first created a CSS flexbox
+        and applied that class to a div to represent the empty grid. Inside the grid, we used the <code>.map</code> function to
+        run 30 times and create each box in the grid. Then, to implement the submit word button, a series of functions 
+        are called to store that word, and to display the word by looping through that string, and accessing each box
+        with <code>document.getClassByName("box")[i]</code>and setting the <code>.innerHTML</code>to the correct letter.
+
+      </h2>
+    </div>
+  </>
 }
 
 export default App
